62c2357cb1807dc818452c658799d05b961eda44
ikjo39 <70360890+ikjo39@users.noreply.github.com>
2022-08-20 20:16:01 +0900

    BOJ-2557 'Hello World' [ 22/08/20 20:16:00 ] by Alub

---
+++ BOJ/2557/2557_1.java
@@ -0,0 +1,10 @@
+/**
+* 메모리: 13916 KB, 시간: 116 ms
+* 2022.08.20
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
