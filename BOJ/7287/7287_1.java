c9ff0d4650cc446839cbcb98d04790e32191f4bf
ikjo39 <70360890+ikjo39@users.noreply.github.com>
2022-08-21 09:17:07 +0900

    BOJ-7287 '등록' [ 22/08/21 09:17:06 ] by Alub

---
+++ BOJ/7287/7287_1.java
@@ -0,0 +1,11 @@
+/**
+* 메모리: 13864 KB, 시간: 116 ms
+* 2022.08.21
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(4);
+        System.out.println("ikjo39");
+    }
+}
