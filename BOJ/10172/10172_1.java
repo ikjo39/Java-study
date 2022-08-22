c59812306fb820ba04b5ae16dec67d13e747e7a4
ikjo39 <70360890+ikjo39@users.noreply.github.com>
2022-08-22 23:29:43 +0900

    BOJ-10172 '개' [ 22/08/22 23:29:43 ] by Alub

---
+++ BOJ/10172/10172_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 14040 KB, 시간: 124 ms
+* 2022.08.22
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
