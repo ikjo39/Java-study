cd41052cfbfd423a7e1ad3c7842ceb0409862f57
ikjo39 <70360890+ikjo39@users.noreply.github.com>
2022-08-22 23:34:08 +0900

    BOJ-10171 '고양이' [ 22/08/22 23:34:08 ] by Alub

---
+++ BOJ/10171/10171_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 13880 KB, 시간: 112 ms
+* 2022.08.22
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                           " )  ( ')\n" +
+                           "(  /  )\n" +
+                           " \\(__)|");
+        
+    }
+}
