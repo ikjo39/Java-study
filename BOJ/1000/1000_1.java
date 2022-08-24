bec8a35561bbe4850f60141cc74cc81e4736c553
ikjo39 <70360890+ikjo39@users.noreply.github.com>
2022-08-24 21:43:38 +0900

    BOJ-1000 'A+B' [ 22/08/24 21:43:38 ] by Alub

---
+++ BOJ/1000/1000_1.java
@@ -0,0 +1,20 @@
+/**
+* 메모리: 17668 KB, 시간: 208 ms
+* 2022.08.24
+* by Alub
+*/
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A+B);
+ 
+		sc.close();
+	}
+}
